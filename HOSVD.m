d6ddd5f4aa50d976d29841c5519ff572a7bec380
Max Yatsenko <maxyatsenko@main-10-230-85-6.tamulink.tamu.edu>
2019-01-22 17:25:35 -0600

    Clean up some files

--- HOSVD.m
+++ HOSVD.m
@@ -3,14 +3,21 @@ close all;
 clear all;
 clc;
 %% Load Data
-%data10=readdata('st5p013.txt','%f');
-%save('10.mat','data10');
-load('datatensor10.mat');
+load('synthTensor.mat');
+%distrcheck(data, 0);
 %data=reshape(data,[],36);
+%tensor4D=zeros(1000,36,2,24);
+% for i=1:24
+%     tensor4D(:,:,1,i)=tensor(1:1000,:,i);
+%     tensor4D(:,:,2,i)=tensor(1001:2000,:,i);
+% end
 %% Plot
-%datay=gety(data);
-%plotn(T(:,:,1));
+datay=gety(data);
+plotn(T(:,:,1));
 %plotn(datay);
+figure();
+slice3(10000.*eventtensor);
+caxis([-10 10])
 %% SVD
 %if closer to a square -> 96
 % [dataSVDy,varY96]=doSVD2(datay,96,0.95);
@@ -18,29 +25,33 @@ load('datatensor10.mat');
 % err=errors(datay,dataSVDy,900);
 %% HOSVD
 %dataMLSVD=reshape(datapy,[],96);
-%[Ue,Se,sve]=mlsvd(T,[n,n]);
-[Ue,Se,sve]=mlsvd(T);
-figure()
-for i = 1:3
-    subplot(1,3,i);
-    plot(sve{i},'.-');
-    xlim([1 length(sve{i})])
-end
-Uetrunc{1} = Ue{1}(:,1:100);
-Uetrunc{2} = Ue{2}(:,1:36);
-Uetrunc{3} = Ue{3}(:,1:10);
-Setrunc = Se(1:100, 1:36, 1:10);
-%dataMLSVD=lmlragen(Ue,Se);
-dataMLSVD=lmlragen(Uetrunc,Setrunc);
-Error=T-dataMLSVD;
-% dataMLSVD=reshape(dataMLSVD,[],12);
-% figure()
-% for i = 1:12
-%     subplot(1,12,i);
-%     plot(dataMLSVD(:,i),'.-');
-%     camroll(-90);
-%     axis off
+%[Ue,Se,sve]=mlsvd(dataMLSVD,[n,n]);
+[Ue,Se,sve]=mlsvd(tensor);
+plotsve(sve);
+[Uetrunc, Setrunc,ntensor]=truncate(Ue,Se,sve,0.005);
+[Uexyz,Sexyz,svexyz]=mlsvd(xyztensor);
+plotsve(svexyz);
+[Uetruncxyz, Setruncxyz,nxyztensor]=truncate(Uexyz,Sexyz,svexyz,0.005);
+newtensor=lmlragen(Uetrunc,Setrunc);
+newxyztensor=lmlragen(Uetruncxyz,Setruncxyz);
+tensor3D=zeros(2000,36,24);
+% for i=1:24
+%     tensor3D(1:1000,:,i)=newtensor(:,:,1,i);
+%     tensor3D(1001:2000,:,i)=newtensor(:,:,2,i);
 % end
+%% Error
+err1=1-(frob(newtensor)/frob(tensor));
+var1=(size(Uetrunc{1},1)*size(Uetrunc{1},2))+(size(Uetrunc{2},1)*...
+size(Uetrunc{2},2))+(size(Uetrunc{3},1)*size(Uetrunc{3},2))+...
+(size(Setrunc,1)*size(Setrunc,2)*size(Setrunc,3));
+reduction1=1-(var1/(size(tensor,1)*size(tensor,2)*size(tensor,3)));
+err2=1-(frob(newxyztensor)/frob(xyztensor));
+var2=(size(Uetruncxyz{1},1)*size(Uetruncxyz{1},2))+...
+(size(Uetruncxyz{2},1)*size(Uetruncxyz{2},2))+(size(Uetruncxyz{3},1)*...
+size(Uetruncxyz{3},2))+(size(Setruncxyz,1)*size(Setruncxyz,2)*...
+size(Setruncxyz,3));
+reduction2=1-(var2/(size(xyztensor,1)*size(xyztensor,2)*...
+size(xyztensor,3)));
 %% Exporting
 % save('dataSVD.mat','Uc','Sc','Vc');
 % save('dataMLSVD.mat','Ue','Se');
--- doSVD2.m
+++
@@ -1,7 +0,0 @@
-function [dataSVD,variables]=doSVD2(data,reshapingTo,precision)
-dataSVD=reshape(data,[],reshapingTo);
-[Uc,Sc,Vc]=svd(dataSVD);
-plotsc(Sc);
-n=cut(diag(Sc),precision);
-[dataSVD,variables]=restoreSVD2(n,Uc,Sc,Vc);
-dataSVD=reshape(dataSVD,[],size(data,2));
